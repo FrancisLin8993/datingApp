893e7dd81722071551063d45ee630b661abd4562
Francis <flin0002@student.monash.edu>
2020-03-06 19:50:58 +1100

    add validation in dto

--- DatingApp.API/DTOs/UserForRegisterDto.cs
+++ DatingApp.API/DTOs/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.DTOs
 {
   public class UserForRegisterDto
   {
+    [Required]
     public string Username { get; set; }
+
+    [Required]
+    [StringLength(8, MinimumLength = 4, ErrorMessage = "Passowrd length must be between 4 and 8")]
     public string Password { get; set; }
   }
 }
