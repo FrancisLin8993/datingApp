4ee491aba3c39f3b69dffd6eaf43f90594bfe9b9
Francis <flin0002@student.monash.edu>
2020-03-07 23:17:42 +1100

    remove redundant code

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -8,29 +8,16 @@ import { HttpClient } from "@angular/common/http";
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.getValues();
-  }
+  ngOnInit() {}
+
   setRegisterMode() {
     this.registerMode = true;
   }
 
-  unsetRegisterMode() {
+  unsetRegisterMode(value: boolean) {
     this.registerMode = false;
-  }
-
-  getValues() {
-    this.http.get("http://localhost:5000/api/values").subscribe(
-      response => {
-        this.values = response;
-      },
-      error => {
-        console.log(error);
-      }
-    );
   }
 }
