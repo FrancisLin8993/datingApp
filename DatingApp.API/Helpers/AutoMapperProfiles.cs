be2df8a761579ba81f301c194d6da7ac645914d2
Francis <flin0002@student.monash.edu>
2020-03-12 20:48:28 +1100

    add a photocontroller in backend

--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -21,6 +21,8 @@ namespace DatingApp.API.Helpers
             .MapFrom(src => src.DateOfBirth.CalculateAge()));
       CreateMap<Photo, PhotosForDetailsDto>();
       CreateMap<UserForUpdateDto, User>();
+      CreateMap<Photo, PhotoForReturnDto>();
+      CreateMap<PhotoForCreationDto, Photo>();
     }
   }
 }
