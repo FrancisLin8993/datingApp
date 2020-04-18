3a1e23b3951790d6c219b9dcf77d5cd80fdf430b
Francis <flin0002@student.monash.edu>
2020-04-18 11:50:20 +1000

    add proxies, add mysql as production db, add prod environment config

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -44,10 +44,7 @@ namespace DatingApp.API.Data
 
     public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
     {
-      var messages = _context.Messages
-        .Include(u => u.Sender).ThenInclude(p => p.Photos)
-        .Include(u => u.Recipient).ThenInclude(p => p.Photos)
-        .AsQueryable();
+      var messages = _context.Messages.AsQueryable();
 
       switch (messageParams.MessageContainer)
       {
@@ -62,15 +59,13 @@ namespace DatingApp.API.Data
           break;
       }
 
-      messages.OrderByDescending(d => d.MessageSent);
+      messages = messages.OrderByDescending(d => d.MessageSent);
       return await PagedList<Message>.CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
     }
 
     public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
     {
       var messages = await _context.Messages
-        .Include(u => u.Sender).ThenInclude(p => p.Photos)
-        .Include(u => u.Recipient).ThenInclude(p => p.Photos)
         .Where(m => m.RecipientId == userId && m.SenderId == recipientId && m.RecipientDeleted == false ||
         m.RecipientId == recipientId && m.SenderId == userId && m.SenderDeleted == false)
         .OrderByDescending(m => m.MessageSent)
@@ -87,13 +82,13 @@ namespace DatingApp.API.Data
 
     public async Task<User> GetUser(int id)
     {
-      var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+      var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
       return user;
     }
 
     public async Task<PagedList<User>> GetUsers(UserParams userParams)
     {
-      var users = _context.Users.Include(p => p.Photos).OrderByDescending(u => u.LastActive).AsQueryable();
+      var users = _context.Users.OrderByDescending(u => u.LastActive).AsQueryable();
       users = users.Where(u => u.Id != userParams.UserId);
       users = users.Where(u => u.Gender == userParams.Gender);
 
@@ -140,8 +135,6 @@ namespace DatingApp.API.Data
     private async Task<IEnumerable<int>> GetUserLikes(int id, bool HasLikers)
     {
       var user = await _context.Users
-          .Include(l => l.Likers)
-          .Include(l => l.Likees)
           .FirstOrDefaultAsync(u => u.Id == id);
 
       if (HasLikers)
